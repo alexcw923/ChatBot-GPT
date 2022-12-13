4ca4dfdf1baa9407470617b4f73b5e6105b5273f
Alex Wong <82024418+alexcw923@users.noreply.github.com>
2022-12-13 13:44:11 +0800

    Update Access

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -1,34 +1,43 @@
-import json
+import os
 import openai
-import torch
+
 
 
 
 class ChatBot():
-    def __init__(self, key, train, name) -> None:
-        openai.api_key = key
-        self.train = train
+    def __init__(self, access) -> None:
+        openai.api_key = os.getenv("OPENAI_API_KEY")
         self.chat_log = ""
-        self.user_name = name
+        self.user_name = None
+        self.access = access
         
-    def train(self):
-        pass
     
-    def response(self, question):
-        self.chat_log += f"You: {question}\n"
-        prompt = f"{self.chat_log}AI:"
-        text = openai.Completion.create(prompt = prompt, 
-                            engine =  "davinci", 
-                            temperature = 0.85,top_p=1, 
-                            frequency_penalty=0, 
-                            presence_penalty=0.7, best_of=2,max_tokens=100,
-                            stop = "\nHuman: ")
-        self.chat_log += f"AI: {text}\n"
+    def response(self, question) -> str:
+        self.chat_log += f"{self.user_name}: {question}\n"
+        prompt = f"{self.chat_log}JOI:"
+        
+        if not self.access:
+            text = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompt,
+                temperature=0.7,
+                max_tokens=3000,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stop = f"\n{self.user_name}: ")
+        else:
+            text = f"I love you!"
+        
+        self.chat_log += f"JOI: {text}\n"
         return text
     
     def chat(self):
+        print("This is your virtual girlfriend. What is your name?")
+        self.user_name = input()
+        print("JOI: Great! My name is JOI. How can I help you?")
         while True:
-            question = input('You:')
+            question = input(f'{self.user_name}:')
             if question == "stop":
                 break
             
