5333d1879723f4d9232e2ca898ab0f9c5d99d256
Alex <carriealex2001@gmail.com>
2022-12-20 16:56:20 -0800

    Fix inputing name bug

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ if __name__ == '__main__':
 
         
     name = input("Hi this is your Virtual Girlfriend. Can I have your name please? ")
-    while name != "":
+    while len(name) == 0:
         name = input("I didn't catch you name. Can I have your name please? ")
         
         
     print(f"It's great to know you {name}. My name is JOI. Let me know what you want to talk about today.")
     option = input("(ask question through text or audio)")
-    while option != "text" or option != "audio":
+    while option not in ["text", "audio"]:
         option = input("(ask question through text or audio)")
     if option == "audio":
         print("(Talk to JOI through your microphone!)")
